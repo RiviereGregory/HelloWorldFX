d06d62aa78e4357c881b56be59b6d86ef48715ea
RiviereGregory <gregory.riviere@laposte.net>
2022-08-01 18:01:24 +0200

    JavaFX Hello sans fxml

--- src/main/java/gri/helloworldfx/HelloApplication.java
+++ src/main/java/gri/helloworldfx/HelloApplication.java
@@ -2,7 +2,13 @@ package gri.helloworldfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +16,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX");
+        greeting.setTextFill(Color.GREEN);
+        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+
+        root.getChildren().add(greeting);
+
+        Scene scene = new Scene(root, 700, 240);
+        stage.setTitle("Hello JavaFX!");
         stage.setScene(scene);
         stage.show();
     }
